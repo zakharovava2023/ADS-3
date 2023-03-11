995a9e48d97307265a93b01c30d1b14cfa02d974
zakharovava2023 <125122557+zakharovava2023@users.noreply.github.com>
2023-03-11 12:50:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int result = 0, left = 0, right = size - 1, middle;
+    while (left < right) {
+        middle = (left + right) / 2;
+        if (value > arr[middle])
+            left = middle + 1;
+        else if (value < arr[middle])
+            right = middle - 1;
+        else if (value == arr[middle]) {
+            int mid_left = middle - 1;
+            int mid_right = middle + 1;
+            result = 1;
+            while ((mid_left >= 0) && (value == arr[mid_left])) {
+                result++;
+                mid_left--;
+            }
+            while ((mid_right < 0) && (value == arr[mid_right])) {
+                result++;
+                mid_right++;
+            }
+            return result;
+        }
+    }
+    return 0; // если ничего не найдено
 }
